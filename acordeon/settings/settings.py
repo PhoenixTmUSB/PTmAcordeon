07fea29b49731b5d0b39cd084df93973d9f9a576
gabo1208 <gabohd@gmail.com>
2017-10-17 17:21:03 -0400

    settings cambiados a postgresql

--- acordeon/settings/settings.py
+++ acordeon/settings/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_phoenix',
+        'USER': 'ptmadmin',
+        'PASSWORD': 'phoenix',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
