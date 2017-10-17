10ec4f854567536e2d6c83519760311b2a5effd0
gabo1208 <gabohd@gmail.com>
2017-10-17 19:00:02 -0400

    acordeon model

--- acordeon/main/models.py
+++ acordeon/main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+
+class Accordion(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, blank=True, null=True)
+    content = models.CharField(max_length=50, blank=True, null=True)
+    width = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        default=Decimal('0.00')
+    )
+    height = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        default=Decimal('0.00')
+    )
+    style = models.TextField(blank=True, null=True)
+    sections = models.ManyToManyField(
+        'Accordion',
+        blank=True,
+        on_delete=models.CASCADE
+    )
