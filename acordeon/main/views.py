a86e3bf3bdbc677d656aaebff55a259ebf8b0d41
edv862 <evalderrama862@gmail.com>
2017-10-19 08:27:07 -0400

    merge de django/app

--- acordeon/main/views.py
+++ acordeon/main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import *
 
-# Create your views here.
+
 def index(request):
     return render(request, 'index.html')
+
 
 def acordeon(request):
     a = Accordion.objects.all()
