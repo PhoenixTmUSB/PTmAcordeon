c44541aca8c58b2006454773f36371baad73fabc
manuggz <manuelggonzalezm@gmail.com>
2017-10-26 21:06:50 -0400

    agregados archivos prueba minesweep - faltan

---
+++ phoenix/minesweep/tests/testCriteriosAceptacion.py
@@ -0,0 +1,287 @@
+from time import sleep
+from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.webdriver import WebDriver
+from minesweep.models import Minesweep
+
+class TestSeleniumCriteriosAceptacion(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestSeleniumCriteriosAceptacion, cls).setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(0)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(TestSeleniumCriteriosAceptacion, cls).tearDownClass()
+
+    def test_criterio_crear_objeto_imagen(self):
+        "Prueba que un usuario pueda crear un objeto-imagen del minesweep"
+        self.selenium.get('%s%s' % (self.live_server_url, '/crear-minesweep/'))
+        enl_modal = self.selenium.find_element_by_css_selector('[data-target="#minesweep-create-modal"]')
+        enl_modal.click()  # Click al enlace para abrir el modal
+
+        minesweep_mdl = Minesweep(
+            tooltip="tooltip",
+            tooltip_style="tooltip_style",
+            content="<img src='/static/img/objeto1.png'>",
+            content_style="content_style",
+            width="123",
+            height="987",
+        )
+        # minesweep_mdl.save()
+
+        form_crear_minesweep = self.selenium.find_element_by_css_selector('form#minesweep-create-form')
+
+        form_crear_minesweep.find_element_by_name('tooltip').send_keys(minesweep_mdl.tooltip)
+        form_crear_minesweep.find_element_by_name('tooltip_style').send_keys(minesweep_mdl.tooltip_style)
+        form_crear_minesweep.find_element_by_name('content').send_keys(minesweep_mdl.content)
+        form_crear_minesweep.find_element_by_name('content_style').send_keys(minesweep_mdl.content_style)
+
+        form_crear_minesweep.find_element_by_name('width').clear()
+        form_crear_minesweep.find_element_by_name('width').send_keys(minesweep_mdl.width)
+
+        form_crear_minesweep.find_element_by_name('height').clear()
+        form_crear_minesweep.find_element_by_name('height').send_keys(minesweep_mdl.height)
+
+        # Guardamos la url actual del usuario antes de crear el minesweep
+        url_antes_guardar_minesweep = self.selenium.current_url
+
+        form_crear_minesweep.find_element_by_css_selector('button[type="submit"]').click()
+
+        # Mientrasque no se haya redirido a la vista donde se muestra el minesweep creado
+        while url_antes_guardar_minesweep == self.selenium.current_url:
+            sleep(1)
+
+        self.assertEqual(self.selenium.current_url, '%s%s' % (self.live_server_url, '/minesweep/'))
+
+        minesweep_mdl_bd = Minesweep.objects.get(tooltip="tooltip")
+
+        self.assertEqual(minesweep_mdl.tooltip,       minesweep_mdl_bd.tooltip)
+        self.assertEqual(minesweep_mdl.tooltip_style, minesweep_mdl_bd.tooltip_style)
+        self.assertEqual(minesweep_mdl.content,       minesweep_mdl_bd.content)
+        self.assertEqual(minesweep_mdl.content_style, minesweep_mdl_bd.content_style)
+        self.assertEqual(minesweep_mdl.width,         minesweep_mdl_bd.width)
+        self.assertEqual(minesweep_mdl.height,        minesweep_mdl_bd.height)
+
+        # Faltaría verificar que la preview del minesweep está bien hecha
+
+        self.assertEqual(self.selenium.find_element_by_css_selector(
+            'div[data-tooltip-content="#minesweep-' + str(minesweep_mdl_bd.minesweep_id) + '"] img').get_attribute('src'), self.live_server_url + '/static/img/objeto1.png')
+
+    def test_criterio_cambiar_imagen_objeto(self):
+        "Prueba que un usuario pueda cambiar la imagen de un objeto del minesweep"
+        minesweep_mdl = Minesweep(
+            tooltip="tooltip",
+            tooltip_style="tooltip_style",
+            content="<img src='/static/img/objeto1.png'>",
+            content_style="content_style",
+            width="123",
+            height="987",
+        )
+        minesweep_mdl.save()
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/minesweep/'))
+        enl_editar = self.selenium.find_element_by_css_selector('div[data-tooltip-content="#minesweep-' + str(minesweep_mdl.minesweep_id) + '"] a.edit-panel-button');
+
+        # Guardamos la url actual del usuario antes de editar el minesweep
+        url_antes_de_accion = self.selenium.current_url
+
+        enl_editar.click()  # Click al enlace para editar el objeto
+
+        # Mientrasque no se haya redirido a la vista donde se edita el minesweep
+        while url_antes_de_accion == self.selenium.current_url:
+            sleep(1) # PEqueña espera a que se cambie de url
+
+        self.assertEqual(self.selenium.current_url, '%s%s' % (self.live_server_url,'/editar-minesweep/' + str(minesweep_mdl.minesweep_id)))
+
+        elem_content_actual = self.selenium.find_element_by_css_selector('textarea#id_content')
+
+        # Verificamos que el contenido sea el actual del modelo
+        self.assertEqual(
+            elem_content_actual.text,
+            minesweep_mdl.content
+        )
+        elem_content_actual.clear()
+        elem_content_actual.send_keys("<img src='/static/img/objeto2.png'>")
+
+        elem_boton_submit = self.selenium.find_element_by_css_selector('button#minesweep-edit-submit')
+
+        elem_boton_submit.click()
+        sleep(2)  #Esperamos que se guarden
+
+        minesweep_mdl_bd = Minesweep.objects.get(tooltip="tooltip")
+
+        # Chequeamos que se hayan hecho los cambios en la base de datos
+        self.assertEqual(minesweep_mdl.tooltip,       minesweep_mdl_bd.tooltip)
+        self.assertEqual(minesweep_mdl.tooltip_style, minesweep_mdl_bd.tooltip_style)
+        self.assertEqual("<img src='/static/img/objeto2.png'>",       minesweep_mdl_bd.content)
+        self.assertEqual(minesweep_mdl.content_style, minesweep_mdl_bd.content_style)
+        self.assertEqual(minesweep_mdl.width,         minesweep_mdl_bd.width)
+        self.assertEqual(minesweep_mdl.height,        minesweep_mdl_bd.height)
+
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/minesweep/'))
+
+        # Faltaría verificar que la preview del minesweep está bien hecha
+
+        self.assertEqual(self.selenium.find_element_by_css_selector(
+            'div[data-tooltip-content="#minesweep-' + str(minesweep_mdl_bd.minesweep_id) + '"] img').get_attribute('src'), self.live_server_url + '/static/img/objeto2.png')
+
+    def test_criterio_eliminar_imagen_objeto(self):
+        "Prueba que un usuario pueda eliminar un objeto del minesweep"
+
+        minesweep_mdl = Minesweep(
+            tooltip="tooltip",
+            tooltip_style="tooltip_style",
+            content="<img src='/static/img/objeto1.png'>",
+            content_style="content_style",
+            width="123",
+            height="987",
+        )
+        minesweep_mdl.save()
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/minesweep/'))
+        enl_eliminar = self.selenium.find_element_by_css_selector('div[data-tooltip-content="#minesweep-' + str(minesweep_mdl.minesweep_id) + '"] a.delete-panel-button');
+
+        enl_eliminar.click()  # Click al enlace para editar el objeto
+
+        sleep(1) # Esperamos que aparezca el alert
+
+        self.selenium.switch_to_alert().accept()
+
+        sleep(2) # Esperamos que se borre
+
+        # Chequeamos que ya no exista
+        self.assertRaises(
+            Minesweep.DoesNotExist,
+            lambda: Minesweep.objects.get(minesweep_id=minesweep_mdl.minesweep_id),
+        )
+
+        # El url debe ser el mismo
+        # Donde se muestran los minesweep
+        # En este caso no se debería mostrar ninguno
+        self.assertEqual(self.selenium.current_url, '%s%s' % (self.live_server_url,'/minesweep/'))
+
+        # Chequeamos que no se esté mostrando el minesweep eliminado
+        self.assertTrue(not self.selenium.find_elements_by_css_selector('div[data-tooltip-content="#minesweep-' + str(minesweep_mdl.minesweep_id) + '"]'))
+
+    def test_criterio_establecer_contenido_extra(self):
+        "Prueba que un usuario pueda establecer el contenido extra de un objeto minesweep"
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/crear-minesweep/'))
+        enl_modal = self.selenium.find_element_by_css_selector('[data-target="#minesweep-create-modal"]')
+        enl_modal.click()  # Click al enlace para abrir el modal
+
+        minesweep_mdl = Minesweep(
+            tooltip="Contenido EXTRA",
+            tooltip_style="tooltip_style",
+            content="<img src='/static/img/objeto1.png'>",
+            content_style="content_style",
+            width="123",
+            height="987",
+        )
+        # minesweep_mdl.save()
+
+        form_crear_minesweep = self.selenium.find_element_by_css_selector('form#minesweep-create-form')
+
+        form_crear_minesweep.find_element_by_name('tooltip').send_keys(minesweep_mdl.tooltip)
+        form_crear_minesweep.find_element_by_name('tooltip_style').send_keys(minesweep_mdl.tooltip_style)
+        form_crear_minesweep.find_element_by_name('content').send_keys(minesweep_mdl.content)
+        form_crear_minesweep.find_element_by_name('content_style').send_keys(minesweep_mdl.content_style)
+
+        form_crear_minesweep.find_element_by_name('width').clear()
+        form_crear_minesweep.find_element_by_name('width').send_keys(minesweep_mdl.width)
+
+        form_crear_minesweep.find_element_by_name('height').clear()
+        form_crear_minesweep.find_element_by_name('height').send_keys(minesweep_mdl.height)
+
+        # Guardamos la url actual del usuario antes de crear el minesweep
+        url_antes_guardar_minesweep = self.selenium.current_url
+
+        form_crear_minesweep.find_element_by_css_selector('button[type="submit"]').click()
+
+        # Mientrasque no se haya redirido a la vista donde se muestra el minesweep creado
+        while url_antes_guardar_minesweep == self.selenium.current_url:
+            sleep(1)
+
+        self.assertEqual(self.selenium.current_url, '%s%s' % (self.live_server_url, '/minesweep/'))
+
+        minesweep_mdl_bd = Minesweep.objects.get(tooltip="Contenido EXTRA")
+
+        self.assertEqual(minesweep_mdl.tooltip,       minesweep_mdl_bd.tooltip)
+        self.assertEqual(minesweep_mdl.tooltip_style, minesweep_mdl_bd.tooltip_style)
+        self.assertEqual(minesweep_mdl.content,       minesweep_mdl_bd.content)
+        self.assertEqual(minesweep_mdl.content_style, minesweep_mdl_bd.content_style)
+        self.assertEqual(minesweep_mdl.width,         minesweep_mdl_bd.width)
+        self.assertEqual(minesweep_mdl.height,        minesweep_mdl_bd.height)
+
+    def test_criterio_cambiar_contenido_extra(self):
+        "Prueba que un usuario pueda cambiar el contenido extra minesweep"
+        minesweep_mdl = Minesweep(
+            tooltip="Contenido EXTRA",
+            tooltip_style="tooltip_style",
+            content="<img src='/static/img/objeto1.png'>",
+            content_style="content_style",
+            width="123",
+            height="987",
+        )
+        minesweep_mdl.save()
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/minesweep/'))
+        enl_editar = self.selenium.find_element_by_css_selector('div[data-tooltip-content="#minesweep-' + str(minesweep_mdl.minesweep_id) + '"] a.edit-panel-button');
+
+        # Guardamos la url actual del usuario antes de editar el minesweep
+        url_antes_de_accion = self.selenium.current_url
+
+        enl_editar.click()  # Click al enlace para editar el objeto
+
+        # Mientrasque no se haya redirido a la vista donde se edita el minesweep
+        while url_antes_de_accion == self.selenium.current_url:
+            sleep(1) # PEqueña espera a que se cambie de url
+
+        self.assertEqual(self.selenium.current_url, '%s%s' % (self.live_server_url,'/editar-minesweep/' + str(minesweep_mdl.minesweep_id)))
+
+        elem_conten_extra_actual = self.selenium.find_element_by_css_selector('textarea#id_tooltip')
+
+        # Verificamos que el contenido sea el actual del modelo
+        self.assertEqual(
+            elem_conten_extra_actual.text,
+            minesweep_mdl.tooltip
+        )
+        elem_conten_extra_actual.clear()
+        elem_conten_extra_actual.send_keys("Contenido EXTRA NUEVO")
+
+        elem_boton_submit = self.selenium.find_element_by_css_selector('button#minesweep-edit-submit')
+
+        elem_boton_submit.click()
+        sleep(2)  #Esperamos que se guarden
+
+        minesweep_mdl_bd = Minesweep.objects.get(tooltip="Contenido EXTRA NUEVO")
+
+        # Chequeamos que se hayan hecho los cambios en la base de datos
+        self.assertEqual("Contenido EXTRA NUEVO",       minesweep_mdl_bd.tooltip)
+        self.assertEqual(minesweep_mdl.tooltip_style, minesweep_mdl_bd.tooltip_style)
+        self.assertEqual(minesweep_mdl.content,       minesweep_mdl_bd.content)
+        self.assertEqual(minesweep_mdl.content_style, minesweep_mdl_bd.content_style)
+        self.assertEqual(minesweep_mdl.width,         minesweep_mdl_bd.width)
+        self.assertEqual(minesweep_mdl.height,        minesweep_mdl_bd.height)
+
+    def test_criterio_establecer_ancho_contenido_extra(self):
+        "Verifica que un usuario pueda establecer el ancho del contenido extra"
+        pass
+
+    def test_criterio_cambiar_ancho_contenido_extra(self):
+        "Verifica que un usuario pueda cambiar el ancho del contenido extra"
+        pass
+
+    def test_criterio_establecer_posicion_contenido_extra(self):
+        "Verifica que un usuario pueda establecer la posicion del contenido extra"
+        pass
+
+    def test_criterio_cambiar_posicion_contenido_extra(self):
+        "Verifica que un usuario pueda cambiar la posicion del contenido extra"
+        pass
+
