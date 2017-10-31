293e56a481d5f570d047202181309f24700dff6a
gabo1208 <gabohd@gmail.com>
2017-10-31 16:12:34 -0400

    patron abstracto

--- phoenix/minesweep/models.py
+++ phoenix/minesweep/models.py
@@ -1,8 +1,9 @@
 import uuid
 from django.db import models
+from accordion.models import PatronAbstract
 
 
-class Minesweep(models.Model):
+class Minesweep(PatronAbstract):
     SIDE_CHOICES = (
         ('top', 'arriba'),
         ('bottom', 'abajo'),
@@ -29,30 +30,6 @@ class Minesweep(models.Model):
         u'Estilos del tooltip',
         blank=True,
         null=True
-    )
-    content = models.TextField(
-        u'Contenido',
-        blank=True,
-        null=True
-    )
-    content_style = models.TextField(
-        u'Estilos del contenido',
-        blank=True,
-        null=True
-    )
-    width = models.CharField(
-        u'Ancho (%)',
-        max_length=50,
-        blank=True,
-        null=True,
-        default='50'
-    )
-    height = models.CharField(
-        u'Alto (px)',
-        max_length=50,
-        blank=True,
-        null=True,
-        default='30'
     )
 
     def __str__(self):
--- phoenix/tab/models.py
+++ phoenix/tab/models.py
@@ -1,13 +1,14 @@
 import uuid
 from django.db import models
+from accordion.models import PatronAbstract
 
 # Create your models here.
-class Tab(models.Model):
+class Tab(PatronAbstract):
     tab_id = models.UUIDField(
         u'Id del tab',
         default=uuid.uuid4,
         editable=False
-    )	
+    )
     title = models.CharField(
         u'Titulo del tab',
         blank=True,
@@ -18,7 +19,7 @@ class Tab(models.Model):
         u'Estilos del titulo',
         blank=True,
         null=True
-    )	
+    )
     content = models.TextField(
         u'Contenido del tab',
         blank=True,
